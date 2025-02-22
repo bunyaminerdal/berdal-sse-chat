80e0721784b3312019964462aeaa7f37907a2440
bunyaminerdal <bunyaminerdal86@gmail.com>
2025-02-22 17:27:40 +0300

    🐛  next auth fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,13 +4,13 @@ import { NextResponse } from "next/server";
 export function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
   if (pathname.startsWith("/chat")) {
-    const token = request.cookies.get("authjs.session-token"); // Retrieve the auth token from cookies
+    const token = request.cookies.get("__Secure-authjs.session-token"); // Retrieve the auth token from cookies
     if (!token) {
       return NextResponse.redirect(new URL("/", request.url));
     }
   }
   if (pathname === "/") {
-    const token = request.cookies.get("authjs.session-token"); // Retrieve the auth token from cookies
+    const token = request.cookies.get("__Secure-authjs.session-token"); // Retrieve the auth token from cookies
     if (token) {
       return NextResponse.redirect(new URL("/chat", request.url));
     }
