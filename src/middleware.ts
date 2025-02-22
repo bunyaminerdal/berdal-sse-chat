df3065bd30f2699a2ca5e65741092a1c7b94f60e
bunyaminerdal <bunyaminerdal86@gmail.com>
2025-02-22 17:32:57 +0300

    🎨 improve session check

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,18 @@
 import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
-
+const cookiesName = process.env.APP_URL
+  ? "__Secure-authjs.session-token"
+  : "authjs.session-token";
 export function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
   if (pathname.startsWith("/chat")) {
-    const token = request.cookies.get("__Secure-authjs.session-token"); // Retrieve the auth token from cookies
+    const token = request.cookies.get(cookiesName); // Retrieve the auth token from cookies
     if (!token) {
       return NextResponse.redirect(new URL("/", request.url));
     }
   }
   if (pathname === "/") {
-    const token = request.cookies.get("__Secure-authjs.session-token"); // Retrieve the auth token from cookies
+    const token = request.cookies.get(cookiesName); // Retrieve the auth token from cookies
     if (token) {
       return NextResponse.redirect(new URL("/chat", request.url));
     }
